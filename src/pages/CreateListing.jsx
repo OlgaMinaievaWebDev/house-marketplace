2f2799e9c7c967db3cb4543d33cf9cfaceb97450
Olga <minaeva9@gmail.com>
2024-12-20 17:23:46 -0500

    onMutate, onSubmit

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -60,9 +60,34 @@ function CreateListing() {
 
   const onSubmit = (e) => {
     e.preventDefault();
+    console.log(formData);
   };
 
-  const onMutate = () => {};
+  const onMutate = (e) => {
+    let boolean = null;
+
+    if (e.target.value === "true") {
+      boolean = true;
+    }
+    if (e.target.value === "false") {
+      boolean = false;
+    }
+
+    //Files
+    if (e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        images: e.target.files,
+      }));
+    }
+    //Text
+    if (!e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        [e.target.id]: boolean ?? e.target.value,
+      }));
+    }
+  };
 
   if (loading) {
     return <Spinner />;
